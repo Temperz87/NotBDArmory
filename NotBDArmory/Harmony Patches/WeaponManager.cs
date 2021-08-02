1c2c7894456b88385deb51023adce38269f6d731
DESKTOP-SM4PJFO\tempe <temperz871@gmail.com>
2021-08-02 19:50:03 -0400

    release moment

---
+++ NotBDArmory/Harmony Patches/WeaponManager.cs
@@ -0,0 +1,26 @@
+﻿using Harmony;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+/*[HarmonyPatch(typeof(WeaponManager))] WIP MP Integration
+[HarmonyPatch("EquipWeapons")]
+public class EquipCustomWeapons
+{
+    public static void Postfix(WeaponManager __instance, Loadout loadout)
+    {
+        if (handler == null)
+            handler = VRHead.instance.gameObject.AddComponent<CustomWeaponHandler>(); // just a handy place to put it
+        handler.EquipCustomWeapons(loadout, __instance);
+    }
+    private static CustomWeaponHandler handler;
+}*/
